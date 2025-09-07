bef340eee6f397729a39043badb914c739f42eeb
tristanmarandin <tristan.marandin@gmail.com>
2025-09-07 17:12:38 +0200

    config update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
